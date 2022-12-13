eca3940e89421ff6b295e74e63efaae277d641c0
Fabian Torres <fabiantorrestech@gmail.com>
2022-12-13 15:27:17 -0600

    website with a video fixed!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home'
+import './App.css';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar/>
-          <Routes>
-            <Route path="/" exact/>
-          </Routes>
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+        </Routes>
       </BrowserRouter>
     </>
   );
---
+++ src/components/HeroSection.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../App.css';
+import './HeroSection.css';
+import { Button } from './Button'
+import videos from '../assets/videos/video-1.mp4'
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src={videos} autoPlay loop muted/>
+      <h1>Welcome to my website!</h1>
+      <p>Here you can find out about me, my projects, see my portfolio, and reach out to me! (:</p>
+      <div className='hero-btns'>
+        <Button className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'>
+            A Test Button
+        </Button>
+        <Button className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'>
+            Another Test Button
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection;
