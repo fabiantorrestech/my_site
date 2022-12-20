7e103e455d27227a6859dede4a8c0b711d392f29
Fabian Torres <fabiantorrestech@gmail.com>
2022-12-19 19:29:59 -0600

    incomplete about-me page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -7,17 +7,23 @@ function AboutMe(){
     <>
     <div className='about-me'>
       <div className='about-me-container'>
-          <div className = 'about-me-contents'>
-            <section className='about-me-title'>
-              <h1> This is my about me page title!</h1>
-            </section>
-            <section className='about-me-info'>
-              <body>
-                This is my story.
-              </body>
-            </section>
-          </div>
+
+        <div className='about-me-title'>
+          <h1>About Me</h1>
         </div>
+
+        <div className='about-me-body'>
+          <body>
+            My name is Fabian Torres. I grew up in the Chicagoland Area.<br/>
+            I'm a University of Illinois at Chicago (UIC) Computer Engineering Alum.<br/>
+            My portfolio ranges from embedded development with C, microcontrollers, to backend
+          </body>
+
+          {/* refreshing on a different page '/my_site/contact_me" does not work? */}
+
+        </div>
+
+      </div>
     </div>
     </>
   )
